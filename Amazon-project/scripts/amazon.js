1bfcc9d1ce0ade5e7fa57d71be6bd9b55421d190
donbhagy <donbhagy@gmail.com>
2025-02-21 12:30:44 +0530

    make the cart quantity interactive

--- Amazon-project/scripts/amazon.js
+++ Amazon-project/scripts/amazon.js
@@ -65,14 +65,20 @@ document.querySelectorAll('.js-add-to-cart')
         }
       });
       if (matchingItem){
-        matchingItem.qyantity += 1;
+        matchingItem.quantity += 1;
       }else{
         cart.push({
           productId : productId,
-          qyantity : 1
+          quantity : 1
         });
       }
       
-      console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
   });
